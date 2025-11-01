c75dc55e40e05f02159f37e3bcc866ab7c01975a
Daniela Baron <baron.daniela@gmail.com>
2025-11-01 16:16:41 -0400

    update user model to reference user recipes

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,16 @@
 class User < ApplicationRecord
   has_secure_password
-  has_many :sessions, dependent: :destroy
 
+  # Associations
+  has_many :sessions, dependent: :destroy
+  has_many :user_recipes, dependent: :destroy
+
+  # Normalization
   normalizes :email_address, with: ->(e) { e.strip.downcase }
+
+  # Validations
+  validates :email_address, presence: true, uniqueness: { case_sensitive: false },
+                            format: { with: URI::MailTo::EMAIL_REGEXP }
+
+  validates :password, length: { minimum: 8 }, if: -> { password.present? }
 end
